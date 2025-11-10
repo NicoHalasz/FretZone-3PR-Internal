f3b83652f251135330294d724c247b1364f57144
NicoHalasz <nico.halasz@my.rongotai.school.nz>
2025-11-10 22:33:05 +1300

    code

--- lessons.php
+++ lessons.php
@@ -4,27 +4,6 @@ $page_title= "Lessons";
 include 'includes/header.php';
 include 'includes/nav.php';
 ?>
-<style>
-#myBtn {
-  display: none;
-  position: fixed;
-  bottom: 20px;
-  right: 30px;
-  z-index: 99;
-  font-size: 25px;
-  border: none;
-  outline: none;
-  background-color: black;
-  color: white;
-  cursor: pointer;
-  padding: 30px;
-  border-radius: 4px;
-}
-
-#myBtn:hover {
-  background-color: #555;
-}
-</style>
 <!-- Title image and text -->
 <img class="lessonimg" src="image/lessons.jpg" alt="Image could not load">
 <h1 class="image-centered title reverse-colour">Lessons</h1>
